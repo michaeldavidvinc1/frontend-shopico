78a2ac0e0e97ba491c5d65ebbbbd3d3079e83279
michaeldavidvinc1 <tawie18102000@gmail.com>
2025-02-26 14:46:18 +0700

    Perbaikan design header

--- src/app/(root)/components/header.tsx
+++ src/app/(root)/components/header.tsx
@@ -27,44 +27,24 @@ export default function HeaderPage({ session, status }: HeaderPageProps) {
         return <HeaderLandingSkeleton />;
     }
     return (
-        <div className='w-full'>
-            <div className='mx-auto px-4 md:px-20 lg:px-24'>
+        <div>
+            <div className='container bg-primary/85 text-white'>
                 <div className='py-1 flex justify-between'>
-                    <div className='flex text-muted-foreground gap-2 items-center'>
+                    <div className='flex gap-2 items-center'>
                         <Barcode className='w-4 h-4' />
                         <p className='uppercase text-xs'>welcome to shopico</p>
                     </div>
                     {session?.user?.role === 'SELLER' ? (
-                        <Link href={ROUTES.DASHBOARD_STORE(data?.data[0]?.slug)} className='text-sm text-muted-foreground'>Dashboard Store</Link>
+                        <Link href={ROUTES.DASHBOARD_STORE(data?.data[0]?.slug)} className='text-sm'>Dashboard Store</Link>
                     ) : (
                         <BecomeSeller />
                     )}
                 </div>
-                <div className='py-4 flex justify-between items-center'>
+            </div>
+            <div className='container'>
+                <div className='py-4 flex justify-around items-center'>
                     <Image src="/image/logo.svg" width={100} height={100} alt='Logo' />
                     <SearchInput />
-                    <div className='flex gap-2 items-center'>
-                        <Headset className='w-10 h-10' />
-                        <div className='flex flex-col'>
-                            <h1 className='font-bold'>+62 822 8723 7236</h1>
-                            <p className='text-sm text-muted-foreground'>customer.service@shopico.com</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className='w-full bg-primary/85 py-4 px-4 md:px-20 lg:px-24 mx-auto text-white'>
-                <div className='flex justify-between items-stretch'>
-                    <div className='flex gap-4 -my-3'>
-                        <Link href="#" className='bg-primary px-4 flex items-center'>
-                            Menu
-                        </Link>
-                        <Link href="#" className='px-4 flex items-center'>
-                            Pages
-                        </Link>
-                        <Link href="#" className='px-4 flex items-center'>
-                            Shop
-                        </Link>
-                    </div>
                     <div className='flex gap-6'>
                         <Link href="#">
                             <User className='w-4 h-4' />
--- src/components/skeleton/SkeletonHeaderLanding.tsx
+++ src/components/skeleton/SkeletonHeaderLanding.tsx
@@ -5,18 +5,20 @@ import { Skeleton } from "../ui/skeleton";
 export const HeaderLandingSkeleton = () => {
   return (
     <div className="w-full">
+      <div className="container bg-primary/85">
+        <div className="py-1 flex justify-between">
+          <div className="flex gap-2 items-center">
+            <Skeleton className="h-4 w-32" />
+          </div>
+          <Skeleton className="h-4 w-24" />
+        </div>
+      </div>
       <div className="mx-auto px-4 md:px-20 lg:px-24">
         {/* Skeleton untuk bagian atas */}
-        <div className="py-1 flex justify-between">
-          <div className="flex text-muted-foreground gap-2 items-center">
-            <Skeleton className="h-2 w-32" />
-          </div>
-          <Skeleton className="h-2 w-24" />
-        </div>
 
         {/* Skeleton untuk bagian tengah */}
-        <div className="py-4 flex justify-between items-center">
-          <Skeleton className="w-16 h-16 rounded-full" />
+        <div className="py-4 flex justify-around items-center">
+          <Skeleton className="w-32 h-8 rounded-lg" />
           <Skeleton className="h-8 w-64" />
           <div className="flex gap-2 items-center">
             <Skeleton className="w-10 h-10 rounded-full" />
@@ -24,22 +26,6 @@ export const HeaderLandingSkeleton = () => {
               <Skeleton className="h-2 w-32" />
               <Skeleton className="h-2 w-48" />
             </div>
-          </div>
-        </div>
-      </div>
-
-      {/* Skeleton untuk bagian bawah */}
-      <div className="w-full bg-primary/85 py-4 px-4 md:px-20 lg:px-24 mx-auto text-white">
-        <div className="flex justify-between items-stretch">
-          <div className="flex gap-4 -my-3">
-            <Skeleton className="h-8 w-16" />
-            <Skeleton className="h-8 w-16" />
-            <Skeleton className="h-8 w-16" />
-          </div>
-          <div className="flex gap-6">
-            <Skeleton className="w-4 h-4" />
-            <Skeleton className="w-4 h-4" />
-            <Skeleton className="w-4 h-4" />
           </div>
         </div>
       </div>
